968b9582d87a0bb1c50600049722ac71501224d4
rallphant <rallphant@gmail.com>
2024-05-08 22:41:24 +0200

    nueva clase - persona
    Se agrega una nueva clase llamada "persona" con 2 atributos y un método constructor.

---
+++ src/persona.java
@@ -0,0 +1,11 @@
+public class persona{
+    private String nombre;
+    private int edad;
+
+
+
+    public persona(String nombre, int edad){
+        this.nombre=nombre;
+        this.edad= edad;
+    }
+}
